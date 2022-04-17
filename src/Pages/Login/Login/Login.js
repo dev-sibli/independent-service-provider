6b691573aa6dde1446caccb49a59d7a8ad22d0e8
dev-sibli <siblisadik13@gmail.com>
2022-04-17 23:18:30 +0600

    login & register page created

---
+++ src/Pages/Login/Login/Login.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    return (
+        <div className='container w-50 mx-auto mt-3'>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button className='w-100 mb-3' variant="primary" type="submit">
+                    Login
+                </Button>
+                <p>New to Traveller? <Link to="/register" className='text-primary text-decoration-none'>Please Register</Link></p>
+                <p>Forget Password? <button className='btn btn-link text-primary pe-auto text-decoration-none'>Reset Password</button> </p>
+            </Form>
+        </div>
+    );
+};
+
+export default Login;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/home">Traveller</Navbar.Brand>
+                    <Navbar.Brand>
+                        <Link className='text-decoration-none' to="/home">Traveller</Link>
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="/home#services">Services</Nav.Link>
+                            <Nav.Link >
+                                <Link className='text-decoration-none' to="/service">Services</Link></Nav.Link>
                             <Nav.Link href="#checkout">Checkout </Nav.Link>
                             <Nav.Link href="#blogs">Blogs</Nav.Link>
-                            <Nav.Link href="/about">About</Nav.Link>
+                            <Nav.Link >
+                                <Link className='text-decoration-none' to="/about">About</Link></Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link eventKey={2} href="#authentication">
-                                Sign In / Out
+                            <Nav.Link eventKey={2} href="/login">
+                                Login
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
